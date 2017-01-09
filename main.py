c11ce7a79a2a591da3cbadfe006485928044378c
Jaxon Kneipp <kneipp.jaxon+github@gmail.com>
2017-01-10 10:58:44 +1100

    Data logging
    Data logging via radio implemented

--- main.py
+++ main.py
@@ -3,4 +3,13 @@
 # Created 10/01/17
 
 from microbit import *
+import math
+
+while True:
+    vals = accelerometer.get_values()
+    print(str(math.sqrt(vals[0]**2+vals[1]**2+vals[2}**2)))
+    
+    sleep(500)
+    
+    
 
