28d169752a1d0d696c0612d3a6fca623b3471295
Aleksa Sarai <cyphar@cyphar.com>
2017-01-11 15:04:40 +1100

    reciever: uncommit merge conflict
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- receiver.py
+++ receiver.py
@@ -3,7 +3,7 @@
 # Created 10/01/17
 
 from microbit import *
-import radio 
+import radio
 
 radio.on()
 radio.config(channel=85)
@@ -11,25 +11,15 @@ radio.config(channel=85)
 counter = 1
 
 while True:
-    
     msg = radio.receive()
-    
+
     if str(msg) == "1":
-<<<<<<< HEAD
-        print('1')
-=======
-        
         counter += 1
         print("count {}".format(counter))
-        
+
     if str(msg) == "game over":
-        
         print("over")
-    
+
     if button_b.was_pressed():
         radio.send("start")
-     
-        
-    
-        
->>>>>>> origin/master
+        print("start")
